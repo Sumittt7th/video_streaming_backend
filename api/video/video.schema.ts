8545ecbdd389e9ee4797201f17575be2de721b78
Sumittt7th <sukumar.75way@gmail.com>
2025-01-18 15:35:19 +0530

    Update video.schema.ts

--- api/video/video.schema.ts
+++ api/video/video.schema.ts
@@ -8,6 +8,7 @@ const VideoSchema = new Schema<IVideo>({
     description: { type: String, required: false },
     url: { type: String, required: true },
     public_id: {type:String,required:true},
+    hlsUrl: { type: String },
     duration: { type: Number, required: false }, 
     access: { type: String, required: true, enum: ["free", "paid"], default: "free" }, 
     viewCount: { type: Number, default: 0 }, 
